d45bcfa8fe9014d1f206be801ba63e6e1eb257c7
mmertdogann <mm3rtdogann@gmail.com>
2021-12-23 13:30:53 +0300

    Add single number challenge

---
+++ data_structures/array/single_number.py
@@ -0,0 +1,14 @@
+'''
+    Leetcode - Single Number: https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/549/
+'''
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        counts = dict()
+        for num in nums:
+            counts[num] = counts.get(num, 0) + 1
+        for key, value in counts.items():
+            if value != 2:
+                return key
+        return 0
