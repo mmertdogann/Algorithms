0cd7d44be0d0358f7d54737ad6107e67629133a4
mmertdogann <mm3rtdogann@gmail.com>
2022-01-17 21:22:15 +0300

    Refactored

--- data_structures/array/two_sum.py
+++ data_structures/array/two_sum.py
@@ -1,17 +1,17 @@
 '''
-    Leetcode - Two Sum: https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/546/
+    Leetcode - Two Sum: https://leetcode.com/problems/two-sum/
 '''
 
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = dict()
         size = len(nums)
-        hash_map = {}
 
         for i in range(size):
             hash_map[nums[i]] = i
 
         for i in range(size):
-            complement = target - nums[i]
-            if complement in hash_map and hash_map[complement] != i:
-                return [i, hash_map[complement]]
+            substraction = target - nums[i]
+            if substraction in hash_map and hash_map[substraction] != i:
+                return [i, hash_map[substraction]]
