60732a1fe0bcd100c93767cf9d8ed1ae8d5677f2
mmertdogann <mm3rtdogann@gmail.com>
2022-01-18 23:51:31 +0300

    Add average of levels challenge

---
+++ data_structures/tree/average_of_levels.py
@@ -0,0 +1,33 @@
+'''
+    Leetcode - Average of Levels in Binary Tree: https://leetcode.com/problems/average-of-levels-in-binary-tree/
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        if root is None:
+            return []
+
+        result = []
+        current_level = [root]
+        while current_level:
+            level_nodes = []
+            next_level = []
+
+            for node in current_level:
+                level_nodes.append(node.val)
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+
+            result.append(sum(level_nodes) / len(level_nodes))
+            current_level = next_level
+        return result
