ac3e14d9d215325304ab54dcf566a82ba8ffa43a
mmertdogann <mm3rtdogann@gmail.com>
2022-01-18 23:51:53 +0300

    Add min depth challenge

---
+++ data_structures/tree/min_depth.py
@@ -0,0 +1,25 @@
+'''
+    Leetcode - Minimum Depth of Binary Tree: https://leetcode.com/problems/minimum-depth-of-binary-tree/
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        else:
+            # if one of the subtree is None, you should return the depth of another subtree.
+            # if all of the subtree is not None, you should return the minimum depth of the two subtrees
+            if root.left is None:
+                return self.minDepth(root.right) + 1
+            elif root.right is None:
+                return self.minDepth(root.left) + 1
+            else:
+                return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
